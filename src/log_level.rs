7799a7af881390dad69f922fbb927cd3e78a79bc
Douglas Fisher <ddfplay@gmail.com>
2018-10-08 18:42:41 -0400

    Updated LogLevel Enum to have to_string fn

--- src/log_level.rs
+++ src/log_level.rs
@@ -1,3 +1,22 @@
 enum LogLevel {
     Debug, Info, Error, Warning, Fatal
 }
+
+impl LogLevel {
+    fn to_string (&self) -> &str { 
+        match self {
+            Debug => "Debug",
+            Info => "Info",
+            Error => "Error",
+            Warning => "Warning",
+            Fatal => "Fatal"
+        }
+    }
+}
+
+#[test]
+fn test_LogLevel() {
+    let message = LogLevel::Error;
+    let message_str = message.to_string();
+    assert!(message_str == "Error");
+}
