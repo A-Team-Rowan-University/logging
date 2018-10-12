b7746b8bbab8da06b359ed33ecdc9061e8a958cc
nick kaegi <kaeginick63@gmail.com>
2018-10-12 12:44:33 -0400

    logges to a file

--- src/log.rs
+++ src/log.rs
@@ -1,4 +1,22 @@
 
-fn log (s: &str, level: LogLevel){
-    println!("{} {}", level.to_string(), s);
+use std::io::Write;
+use std::fs::OpenOptions;
+
+
+fn log (s: &str, erlog: LogLevel){
+
+    //println!("{} {}", erlog.to_string(), s); 
+
+    //fs::write( "errorLog.txt",erlog.to_string());
+    //fs::write( "errorLog.txt",s);
+
+    let input = format!("{} {}",s,erlog.to_string());
+
+    let mut file = OpenOptions::new()
+            .write(true)
+            .create(true)
+            .append(true)
+            .open("Nickslist").unwrap();
+
+        file.write(input);
 }
